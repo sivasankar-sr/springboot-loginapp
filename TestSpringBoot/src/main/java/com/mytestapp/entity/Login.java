43aaff00963fd402eae31105ffadd3487f15d542
sivasankar-sr <sivasankar.sr07@gmail.com>
2020-09-21 16:30:34 +0530

    integated with jpa

---
+++ TestSpringBoot/src/main/java/com/mytestapp/entity/Login.java
@@ -0,0 +1,51 @@
+package com.mytestapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Login {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "LoginEntity [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+	private String username;
+    public Login(Long id, String username, String password) {
+		
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+	public Login() {
+		
+	}
+	private String password;
+
+}
